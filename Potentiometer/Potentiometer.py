406b4c5bbc7d60fc02ced34d2ee640c303c853a2
AndyStobirski <andy@stobirski.com>
2022-10-22 14:12:00 +0100

    Updated

--- Potentiometer/Potentiometer.py
+++ Potentiometer/Potentiometer.py
@@ -1,21 +1,9 @@
 from machine import ADC, Pin
 import time
-from libs.neopixel import Neopixel
-
-
-numpix = 12	# Number of pixels on the strip												
-ring = Neopixel(numpix, 0, 28, "GRB")
-
-ring.fill((255,255,255))
-ring.show()
 
 
 adc = ADC(Pin(27))
 
 while True:
-    print(adc.read_u16()/256)
-    ring.brightness(int(adc.read_u16()/256))
-    ring.fill((255,255,255))
-    
-    ring.show()
+    print(adc.read_u16()/256) # convert to a value between 1 and 255
     time.sleep(1)
